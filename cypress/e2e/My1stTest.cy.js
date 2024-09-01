5dfd305b5981a26d82d8b12988f8e6d16aa1ebb6
sahoo2676 <sksahoo12475@gmail.com>
2024-09-01 17:37:55 +0530

    first cs commit

---
+++ cypress/e2e/My1stTest.cy.js
@@ -0,0 +1,14 @@
+describe('My1stTest', () => {
+it('verify tttle positive', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM')
+})
+
+it('verify tittle negative', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM123')
+  
+    
+})
+
+})
